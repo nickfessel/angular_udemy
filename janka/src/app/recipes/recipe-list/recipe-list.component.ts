08692e5cf489250d9db91b4304dbcc09065dfdf1
Nick Fessel <nfessel@gmail.com>
2021-10-17 18:03:34 -0400

    More class progress.

--- janka/src/app/recipes/recipe-list/recipe-list.component.ts
+++ janka/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, OnInit, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +6,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes:Recipe[] = [
     new Recipe("A Test Recipe","This is simply a test.","https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg"),
     new Recipe("A Test Recipe 2","This is simply a test 2.","https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg")
@@ -16,4 +18,8 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+
+  }
 }
